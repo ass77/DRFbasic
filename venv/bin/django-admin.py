166798ad2642ed6400749c7688e64634b9a80ca0
AdriShahri <adrishahri@gmail.com>
2020-09-24 07:11:29 +0800

    Familiarization DRF

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/adrishahribinsantosoh/PycharmProjects/RestBasic/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
